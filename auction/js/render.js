616b37689ea8e9159222698a3957df6fbed9731c
yoshikier <124565124@qq.com>
2016-03-18 17:58:45 +0800

    del
    del bot

--- auction/js/render.js
+++
@@ -1,8 +0,0 @@
-var scale = 1;
-var css_width = 320;
-var page_height = $(window).height();
-var page_width = $(window).width();
-scale = page_height / 508;
-css_width = page_width / scale; //css宽度，跟页面宽度相同了
-
-//$('body').css("-webkit-transform", " scale(" + scale + ","+scale+")");
